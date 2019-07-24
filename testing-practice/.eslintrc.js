5cbfc4fe043326b32a59abebe33648c32eed50b4
youngsoohan <amazingmobdev@gmail.com>
2019-07-25 01:41:30 +0900

    tsconfig

--- testing-practice/.eslintrc.js
+++ testing-practice/.eslintrc.js
@@ -4,51 +4,19 @@ module.exports = {
     es6: true,
     jest: true,
   },
-  extends: ['standard', 'prettier'],
-  parser: '@typescript-eslint/parser',
+  extends: ["standard", "plugin:react/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+  plugins: ["react"],
+  rules: {},
 };
-
-// module.exports = {
-//   extends: ['standard', 'prettier'],
-//   parser: '@typescript-eslint/parser',
-//   env: {
-//     browser: true,
-//     jest: true,
-//   },
-//   parserOptions: {
-//     ecmaVersion: 2017,
-//     sourceType: 'module',
-//   },
-//   rules: {
-//     'no-unused-vars': 0,
-//     'comma-dangle': ['error', 'always-multiline'],
-//     semi: [2, 'always'],
-//     'space-before-function-paren': ['error', 'never'],
-//     'no-new-object': 'error',
-//     'no-array-constructor': 'error',
-//     'space-before-function-paren': [
-//       'error',
-//       {
-//         anonymous: 'never',
-//         named: 'never',
-//         asyncArrow: 'always',
-//       },
-//     ],
-//   },
-// }
